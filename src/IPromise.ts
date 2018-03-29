afcd7c94db09254dda07e71668ed39e0a230933e
Patrick van der Linden <patrickvd_linden@hotmail.com>
2018-03-29 13:39:23 +0200

    Add OptionsScreen

--- src/IPromise.ts
+++ src/IPromise.ts
@@ -1,5 +1,3 @@
 interface IPromise<T> {
-    fulfill(onfulfill: (value?: T) => void): IPromise<T>;
-    reject(onreject: (reason?: any) => void): IPromise<T>;
     then(onfulfill: (value?: T) => void, onreject?: (reason?: any) => void): IPromise<T>;
 }
